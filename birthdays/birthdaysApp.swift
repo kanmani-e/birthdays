c065daf0606a5c4bd1adc574d7f8936c726e36b8
Kanmani Elangovan <kanmani.elan@gmail.com>
2025-07-11 10:10:51 -0400

    Initial Commit

---
+++ birthdays/birthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  birthdaysApp.swift
+//  birthdays
+//
+//  Created by Kanmani Elangovan on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct birthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
